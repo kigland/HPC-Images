fd4be73210d34248c33035e9ad336ea5e7604f39
Xiang Shi <realkevin@tutanota.com>
2025-04-17 02:33:41 +0800

    Update start-nb-with-ssh.sh

--- start-nb-with-ssh.sh
+++ start-nb-with-ssh.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) Xiang Shi (KevinZonda) & KigLand
+
+set -e
 
 if [ -n "$JUPYTER_TOKEN" ]; then
     echo "jovyan:$JUPYTER_TOKEN" | sudo chpasswd
@@ -6,4 +9,9 @@ fi
 
 sudo /usr/sbin/sshd -D &
 
-exec start-notebook.sh
+# Check if NB_VAR_BASE_URL is set
+if [ -z "$NB_VAR_BASE_URL" ]; then
+    exec start-notebook.sh
+else
+    exec start-notebook.sh --NotebookApp.base_url="$NB_VAR_BASE_URL"
+fi
